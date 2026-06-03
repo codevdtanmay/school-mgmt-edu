13db8d3cb20d8755bf1294159821c2ea90e2b261
TanmayPardhi <myselftanmay8@gmail.com>
2026-06-03 22:28:24 +0530

    Complete Authentication Done

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -11,6 +11,7 @@ const register = async(req,res) => {
     const isUserAlreadyExist = await userModel.findOne({email});
     if (isUserAlreadyExist){
         return res.status(409).json({
+            
             message: "User Already Exists"
         })
     } 
@@ -40,9 +41,107 @@ const register = async(req,res) => {
 
 
    } catch (error) {
-    console.log("Register User Issue");
-    console.log(error)
+  if (error.name === "ValidationError") {
+    return res.status(400).json({
+      success: false,
+      message: Object.values(error.errors)[0].message
+    });
+  }
+
+  return res.status(500).json({
+    success: false,
+    message: "Internal Server Error"
+  });
+}
    }
+
+const login = async(req,res) => {
+    try {
+     const {email, password} = req.body;
+     
+     if(!email || !password){
+        return res.status(400).json({
+            success : false,
+            message: "Email and password are required"
+        })
+     }
+
+     const user = await userModel.findOne({email});
+     if(!user){
+        return res.status(401).json({
+            success: false,
+            message: "Invalid Credentials"
+        })
+     }
+
+     const isMatch = await bcrypt.compare(
+        password,
+        user.password
+     );
+
+     if(!isMatch){
+        return res.status(401).json({
+            success: false,
+            message: "Invalid Credentials"
+        })
+     }
+
+    
+     const token = jwt.sign({
+        id: user._id,
+        role: user.role
+     },process.env.JWT_SECRET,{
+        expiresIn: '7d'
+     })
+     
+     res.cookie("token", token, {
+        httpOnly : true
+     })
+     
+
+    res.status(200).json({
+    success: true,
+     message: "Login successful",
+    user: {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role
+  }
+});
+    }
+
+catch (error) {
+    console.error("Login User Issue:", error);
+
+    if (error.name === "ValidationError") {
+        return res.status(400).json({
+            success: false,
+            message: error.message
+        });
+    }
+
+    return res.status(500).json({
+        success: false,
+        message: "Internal Server Error"
+    });
+}
+}
+  
+const getme = async(req, res) => {
+    const user = await userModel.findById(
+        req.user.id
+    ).select("-password")
+
+    res.status(200).json({
+        success:true,
+          user: {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role
+  }
+    })
 }
 
-export default {register}
+export default {register, login, getme}
--- backend/src/routes/authRoute.route.js
+++ backend/src/routes/authRoute.route.js
@@ -1,9 +1,11 @@
 
 import authController from "../controllers/authController.js";
 import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post(("/register"), authController.register)
-
+router.post(("/login"), authController.login)
+router.get(("/me"), authMiddleware, authController.getme)
 export default router
